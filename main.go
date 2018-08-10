5f7550876df9a8042bdf563b8d0a47e81097294e
Denis Mazur <denismazur8@gmail.com>
2018-08-11 02:37:35 +0300

    heroku ready

--- main.go
+++ main.go
@@ -10,19 +10,19 @@ import (
 var files, _ = filepath.Glob("templates/*")
 var templates, _ = template.New("").ParseFiles(files...)
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", struct{}{})
-}
-
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
+
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 
-	log.Println(templates)
-
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":80", nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", struct{}{})
 }
