c798f2fa55341e0afe202c805e421d798fc21b03
Denis Mazur <denismazur8@gmail.com>
2018-08-11 03:03:11 +0300

    feat: change message text

--- static/script.js
+++ static/script.js
@@ -1,13 +1,19 @@
 var heart;
+var message;
 
 window.onload = function() {
     heart = document.getElementById("heart")
+
+    message = document.getElementById("message")
+    message.innerText = "When I don't see you"
 }
 
 function onClick(checkbox) {
     if (checkbox.checked) {
         heart.style.animation = ".6s infinite beatHeart"
+        message.innerText = "When I see you"
     } else {
         heart.style.animation = "1s infinite beatHeart"
+        message.innerText = "When I don't see you"
     }
 }
